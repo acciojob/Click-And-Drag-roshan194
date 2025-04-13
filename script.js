f3e51c2e609ccf2939f645679884605d37d6294f
Roshan <71066704+roshan194@users.noreply.github.com>
2025-04-13 22:14:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
+// script.js
+
 const slider = document.querySelector('.items');
+
 let isDown = false;
 let startX;
 let scrollLeft;
 
 slider.addEventListener('mousedown', (e) => {
   isDown = true;
+  slider.classList.add('active');
   startX = e.pageX - slider.offsetLeft;
   scrollLeft = slider.scrollLeft;
 });
 
-slider.addEventListener('mouseleave', () => isDown = false);
-slider.addEventListener('mouseup', () => isDown = false);
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
 
 slider.addEventListener('mousemove', (e) => {
   if (!isDown) return;
   e.preventDefault();
   const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 2; // speed multiplier
+  const walk = (x - startX) * 1.5; // Sensitivity
   slider.scrollLeft = scrollLeft - walk;
 });
